9fe327436bed553d80f471134d1c2eff8249be7b
Caleb B. <97490793+FordPIU@users.noreply.github.com>
2024-08-03 23:56:41 -0700

    Update propertiesThread.lua

--- Client/propertiesThread.lua
+++ Client/propertiesThread.lua
@@ -1,9 +1,21 @@
 Citizen.CreateThread(function()
     while true do
+        -- 1 TPS
         Wait(1000)
 
         for _, veh in pairs(GetGamePool("CVehicle")) do
+            -- Updated properties for vehicles that server says needs it
+            -- I honestly hate this method but too far along and too tired.
             if Entity(veh).state.NeedsPropertiesSet ~= nil then
+                -- Safeguard to make sure no issues with fivems shit networking happens
+                if not NetworkHasControlOfEntity(veh) then
+                    repeat
+                        Wait(0)
+                        NetworkRequestControlOfEntity(veh)
+                    until NetworkHasControlOfEntity(veh)
+                end
+
+                -- This is just kinda ugly, fuck networking
                 SetVehicleProperties(veh, Entity(veh).state.NeedsPropertiesSet)
                 TriggerServerEvent("CR.PV:PropertiesSet", NetworkGetNetworkIdFromEntity(veh))
             end
