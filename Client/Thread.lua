a30e25bbf0a0fb6e144237657ff785afc5ce686a
Caleb B. <97490793+FordPIU@users.noreply.github.com>
2024-08-08 01:32:57 -0700

    More logging

--- Client/Thread.lua
+++ Client/Thread.lua
@@ -5,21 +5,29 @@ Citizen.CreateThread(function()
         local propertiesSet = {}
         local propertiesUpdate = {}
 
+        print("Starting vehicle property check and update")
+
         for _, v in ipairs(GetGamePool("CVehicle")) do
             local vState = Entity(v).state
 
             if vState.isPersistent then
-                -- Needs Properties Set
                 if vState.nProperties then
+                    print("Setting properties for vehicle with Net ID: " .. VehToNet(v))
                     SetVehicleProperties(v, vState.pProperties, vState.pId)
                     propertiesSet[VehToNet(v)] = true
                 else
+                    print("Updating properties for vehicle with Net ID: " .. VehToNet(v))
                     propertiesUpdate[VehToNet(v)] = GetVehicleProperties(v)
                 end
+            else
+                warn("Vehicle with entity ID: " .. v .. " is not persistent, skipping property check")
             end
         end
 
+        print("Triggering server event for properties set")
         TriggerServerEvent("CR.PV:PropertiesSet", propertiesSet)
+
+        print("Triggering server event for properties update")
         TriggerServerEvent("CR.PV:PropertiesUpdate", propertiesUpdate)
     end
 end)
