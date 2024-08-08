52ebd9dda8c017324f2084daefb61f7d0331b1de
Caleb B. <97490793+FordPIU@users.noreply.github.com>
2024-08-08 01:31:55 -0700

    More logging

--- Server/Manager.lua
+++ Server/Manager.lua
@@ -7,18 +7,23 @@ function LoadVehicleData(resourceName)
     local vehiclesJson = LoadResourceFile(resourceName or GetCurrentResourceName(), "vehicles.json")
 
     if vehiclesJson ~= nil then
+        print("Loading vehicle data from vehicles.json")
         Vehicles = TrimVehiclesJson(vehiclesJson)
     else
-        warn("No file: vehicles.json")
+        warn("No file: vehicles.json found while loading vehicle data")
     end
 
     GlobalState.spVehicles = GlobalState.spVehicles or {}
 
+    print("Deleting all persistent vehicles")
     DeleteAllPersistentVehicles()
+
+    print("Spawning all persistent vehicles")
     SpawnAllPersistentVehicles()
 end
 
 function SaveVehicleData(resourceName)
+    print("Saving vehicle data to vehicles.json")
     SaveResourceFile(resourceName or GetCurrentResourceName(), "vehicles.json", json.encode(Vehicles), -1)
 end
 
@@ -29,8 +34,10 @@ function SpawnVehicle(vehicleId)
     if GlobalState.spVehicles[vehicleId] == nil then
         local vehicleData = Vehicles[vehicleId]
         local position = vehicleData.matrix.position
-        local vehicle = CreateVehicleServerSetter(vehicleData.model, vehicleData.type, position.x, position.y,
-            position.z, vehicleData.matrix.heading)
+        local vehicle = CreateVehicleServerSetter(vehicleData.model, vehicleData.type, position.x, position.y, position
+        .z, vehicleData.matrix.heading)
+
+        print("Spawning vehicle with Vehicle ID: " .. vehicleId)
 
         Entity(vehicle).state.isPersistent = true
         Entity(vehicle).state.pProperties = vehicleData
@@ -38,11 +45,15 @@ function SpawnVehicle(vehicleId)
         Entity(vehicle).state.pId = vehicleId
 
         GlobalState.spVehicles[vehicleId] = true
+    else
+        warn("Vehicle with Vehicle ID: " .. vehicleId .. " is already persistent")
     end
 end
 
 function NewVehicle(vehicle)
     local vehicleId = GetVehicleUID(vehicle)
+
+    print("Registering new vehicle with Vehicle ID: " .. vehicleId)
 
     Vehicles[vehicleId] = true
 
@@ -52,7 +63,11 @@ function NewVehicle(vehicle)
 end
 
 function UpdateVehicle(vehicle, properties)
-    Vehicles[GetVehicleUID(vehicle)] = properties
+    local vehicleId = GetVehicleUID(vehicle)
+
+    print("Updating vehicle with Vehicle ID: " .. vehicleId)
+
+    Vehicles[vehicleId] = properties
 
     Entity(vehicle).state.pProperties = properties
     SaveVehicleData()
@@ -60,6 +75,8 @@ end
 
 function ForgetVehicle(vehicle)
     local vehicleId = GetVehicleUID(vehicle)
+
+    print("Forgetting vehicle with Vehicle ID: " .. vehicleId)
 
     Vehicles[vehicleId] = nil
 
@@ -69,16 +86,28 @@ function ForgetVehicle(vehicle)
 end
 
 function IsVehiclePersistent(vehicle)
-    if Vehicles[GetVehicleUID(vehicle)] then return true else return false end
+    local vehicleId = GetVehicleUID(vehicle)
+    local isPersistent = Vehicles[vehicleId] ~= nil
+
+    if isPersistent then
+        print("Vehicle with Vehicle ID: " .. vehicleId .. " is persistent")
+    else
+        print("Vehicle with Vehicle ID: " .. vehicleId .. " is not persistent")
+    end
+
+    return isPersistent
 end
 
 function DeleteAllPersistentVehicles()
     local vehicles = GetAllVehicles()
 
-    ---@diagnostic disable-next-line: param-type-mismatch
+    print("Deleting all persistent vehicles")
+
     for _, vehicle in ipairs(vehicles) do
         if IsVehiclePersistent(vehicle) then
             local vehicleId = GetVehicleUID(vehicle)
+
+            print("Deleting persistent vehicle with Vehicle ID: " .. vehicleId)
 
             Entity(vehicle).state.isPersistent = false
             DeleteEntity(vehicle)
@@ -89,6 +118,8 @@ function DeleteAllPersistentVehicles()
 end
 
 function SpawnAllPersistentVehicles()
+    print("Spawning all persistent vehicles")
+
     for vehicleId, _ in pairs(Vehicles) do
         SpawnVehicle(vehicleId)
     end
