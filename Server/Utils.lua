4503583379af03348c142e3d22a735f455dceb0d
Caleb B. <97490793+FordPIU@users.noreply.github.com>
2024-08-08 01:32:28 -0700

    More logging

--- Server/Utils.lua
+++ Server/Utils.lua
@@ -2,16 +2,20 @@ function GetVehicleUID(vehicle)
     local plateText = GetVehicleNumberPlateText(vehicle)
     local plateIndex = GetVehicleNumberPlateTextIndex(vehicle)
 
+    print("Getting UID for vehicle with plate: " .. plateText .. " and plate index: " .. plateIndex)
+
     return plateIndex .. "-" .. plateText
 end
 
 exports("GetVehicleUID", GetVehicleUID)
 
 function TrimVehiclesJson(vehiclesJson)
+    print("Trimming vehicle JSON data")
     local decodedVehicles = json.decode(vehiclesJson)
 
     for vehicleUID, vehicleProperties in pairs(decodedVehicles) do
         if vehicleProperties == true then
+            print("Removing vehicle with UID: " .. vehicleUID .. " from the list")
             decodedVehicles[vehicleUID] = nil
         end
     end
@@ -19,27 +23,10 @@ function TrimVehiclesJson(vehiclesJson)
     return decodedVehicles
 end
 
-Citizen.CreateThread(function()
-    while true do
-        Wait(100)
-        local playerList = {}
-
-        for _, playerId in ipairs(GetPlayers()) do
-            playerId = tonumber(playerId)
-            local ped = GetPlayerPed(playerId)
-            local coord = GetEntityCoords(ped)
-
-            if DoesEntityExist(ped) and coord ~= nil then
-                playerList[playerId] = coord
-            end
-        end
-
-        TriggerClientEvent("CR.PV:Playerlist", -1, playerList)
-    end
-end)
-
 RegisterCommand("dvall", function()
+    print("Deleting all vehicles")
     for _, v in ipairs(GetGamePool("CVehicle")) do
         DeleteEntity(v)
+        print("Deleted vehicle with entity ID: " .. v)
     end
 end, false)
